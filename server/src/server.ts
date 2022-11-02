48e81bf462e242db9cf055a4d68c1cee06f9972f
alynevieira <alynealicevieira42@gmail.com>
2022-11-02 13:31:04 -0300

    First commit

---
+++ server/src/server.ts
@@ -0,0 +1,69 @@
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+
+import { PrismaClient } from '@prisma/client'
+
+import ShortUniqueId from 'short-unique-id'
+import { z } from 'zod'
+
+// Vai mostrar os logs no terminal de todas as query que são executadas no bd
+const prisma = new PrismaClient({
+    log: ['query'],
+})
+
+async function bootstrap() {
+    // Solta logs para a aplicação
+    const fastify = Fastify({
+        logger: true
+    })
+
+    await fastify.register(cors, {
+        origin: true
+    })
+
+    fastify.get('/pools/count', async () => {
+        const count = await prisma.pool.count()
+
+        return { count }
+    })
+
+    fastify.post('/pools', async (request, response) => {
+        const createPoolBody = z.object({
+            title: z.string()
+        })
+
+        try {
+            const { title } = createPoolBody.parse(request.body)
+            const generate = new ShortUniqueId({ length: 6 })
+            const code = String(generate()).toUpperCase()
+
+            await prisma.pool.create({
+                data: {
+                    title,
+                    code
+                }
+            })
+
+            return response.status(201).send({ code })
+        } catch {
+            return response.status(500).send({ message: "O título do bolão não pode ser vazio."})
+        }
+
+    })
+
+    fastify.get('/users/count', async () => {
+        const count = await prisma.user.count()
+
+        return { count }
+    })
+
+    fastify.get('/guesses/count', async () => {
+        const count = await prisma.guess.count()
+
+        return { count }
+    })
+
+    await fastify.listen({ port: 3333, host: '0.0.0.0' })
+}
+
+bootstrap()
