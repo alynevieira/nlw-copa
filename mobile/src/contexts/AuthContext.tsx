c2175a48f949a83f9c6b9be099bc153206a204a5
alynevieira <alynealicevieira42@gmail.com>
2022-11-02 20:37:06 -0300

    Implementando uma parte da autenticação no AuthContext, conectando com o google

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,4 +1,12 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState, useEffect } from "react";
+
+import * as Google from 'expo-auth-session/providers/google';
+import * as AuthSession from 'expo-auth-session';
+import * as WebBrowser from 'expo-web-browser';
+
+import { TOKEN_GOOGLE_AUTH } from "@env";
+
+WebBrowser.maybeCompleteAuthSession();
 
 interface UserProps {
     name: string;
@@ -7,6 +15,7 @@ interface UserProps {
 
 export interface AuthContextDataProps {
     user: UserProps;
+    isUserLoading: boolean;
     signIn: () => Promise<void>;
 }
 
@@ -17,18 +26,43 @@ interface AuthProviderProps {
 export const AuthContext = createContext({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthProviderProps) {
+    const [user, setUser] = useState<UserProps>({} as UserProps)
+    const [isUserLoading, setIsUserLoading] = useState(false);
+
+    const [request, response, promptAsync] = Google.useAuthRequest({
+        clientId: TOKEN_GOOGLE_AUTH,
+        redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+        scopes: ['profile', 'email']
+    })
 
     async function signIn() {
-        console.log('vamos logar')
+        try {
+            setIsUserLoading(true);
+            await promptAsync();
+        } catch (error) {
+            console.log(error);
+
+            throw error;
+        } finally {
+            setIsUserLoading(false);
+        }
     }
+
+    async function signInWithGoogle(access_token: string) {
+        console.log("Token de auth ->", access_token)
+    }
+
+    useEffect(() => {
+        if(response?.type === 'success' && response.authentication?.accessToken) {
+            signInWithGoogle(response.authentication.accessToken);
+        }
+    }, [response]);
 
     return (
         <AuthContext.Provider value={{
             signIn,
-            user: {
-                name: 'Alice',
-                avatarUrl: 'https://github.com/alynevieira.png'
-            }
+            isUserLoading,
+            user
         }}>
             { children }
         </AuthContext.Provider>
