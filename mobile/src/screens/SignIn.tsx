ddf724ba389c6544f18e230f65f579c44c5e1d59
alynevieira <alynealicevieira42@gmail.com>
2022-11-02 17:18:11 -0300

    Criando e implementando o contexto de autenticação

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -4,8 +4,11 @@ import { Button } from "../components/Button";
 import Logo from '../assets/logo.svg';
 
 import { Fontisto } from '@expo/vector-icons';
+import { useAuth } from "../hooks/useAuth";
 
 export function SignIn() {
+    const { signIn } = useAuth();
+
     return (
         <Center flex={1} bgColor="gray.900" p={7}>
             <Logo width={212} height={40}  />
@@ -16,6 +19,7 @@ export function SignIn() {
                 type="SECONDARY"
                 isLoading={false}
                 mt={12}
+                onPress={signIn}
             />
 
             <Text color="white" textAlign="center" mt={4}>
